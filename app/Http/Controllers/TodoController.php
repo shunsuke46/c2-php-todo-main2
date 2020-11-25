9728f6bc8702ca8b8c29e68cb45c2e6884eb1222
yukaaaaaaa <19s019yuka@gmail.com>
2020-11-25 12:24:10 +0900

    削除機能追加

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -99,6 +99,7 @@ class TodoController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $todo = Auth::user()->todos()->findOrFail($id);
+        $todo->delete();
     }
 }
